b5ecff81057cadc434c8408b878b8ea8359029cd
ESSARRAJ <essarraj.fouad@gmail.com>
2016-12-10 11:39:27 +0000

    aa

--- Tâches/Besoin technique/Enregistrer_image/tp_photo/tp_photo/test_img.sql
+++
@@ -1,9 +0,0 @@
-create database test 
-use test
-create table tes_img
-(
-id int primary key ,
-img varbinary(max),
-descri varchar(40)
-)
-select*from tes_img
--- Tâches/Besoin technique/validateur de formulaire/Test Validateur/test.sql
+++
@@ -1,8 +0,0 @@
-create database test
-use test
-create table test(cne nvarchar(20)primary key,nom nvarchar(20),prenom nvarchar(20),date_naissance datetime,lieu_naissance nvarchar(20) )
-insert into test values ('AE121547','Salmi','Amin','22/06/1992','Tanger')
-insert into test values ('KB121547','Idrisi','Khalid','30/01/1990','Casa')
-insert into test values ('K121547','Alami','Youssef','11/04/1996','Fes')
-select * from test
-delete test
